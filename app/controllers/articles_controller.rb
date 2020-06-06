d63e2bcf0d011a54b6441b385a031ed2779a4db6
Tanuj Ahuja <tanujahuja1999@gmail.com>
2020-06-07 01:06:35 +0530

    chrome on heroku

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,8 @@
 class ArticlesController < ApplicationController
 
 	# get the chromedriver
-	chromedriver_path = File.join(File.absolute_path('../..', File.dirname(__FILE__)),"chromedriver")
-	Selenium::WebDriver::Chrome.driver_path = chromedriver_path
+	# chromedriver_path = File.join(File.absolute_path('../..', File.dirname(__FILE__)),"chromedriver")
+	# Selenium::WebDriver::Chrome.driver_path = chromedriver_path
 	# our browser variable
 	@@browser = nil
 	# time to crawl
