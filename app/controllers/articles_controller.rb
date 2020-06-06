44282b2b2091debb0013fef79fa91a11eae71efb
Tanuj Ahuja <tanujahuja1999@gmail.com>
2020-06-06 16:11:28 +0530

    clean code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,38 +1,51 @@
 class ArticlesController < ApplicationController
 
+	# get the chromedriver
 	chromedriver_path = File.join(File.absolute_path('../..', File.dirname(__FILE__)),"chromedriver")
 	Selenium::WebDriver::Chrome.driver_path = chromedriver_path
+	# our browser variable
 	@@browser = nil
+	# time to crawl
 	@@time_taken_to_crawl = 0
 
 
 	def new
 	end
 
+	# display the last inserted articles
 	def index
-		@articles=Article.all
-		@last_inserted = session[:last_inserted]
+		@articles=Article.last(session[:last_inserted])
+		# tttc => time taken to crawl
 		@tttc = @@time_taken_to_crawl
+		# get search history
 		@history=History.all
 	end
 
+	# display details of a particular article
 	def show
 		@article=Article.find(params[:id])
 	end
 
+	# get next 10 articles for a particular tag
 	def next
+		# scroll the browser down to load next 10 articles
 		@@browser.send_keys :control, :end
 		sleep(0.7)
+		# session variable to store the number of articles inserted during latest crawl
 		session[:last_inserted] = 0
 		crawl
 		redirect_to articles_path
 	end
 
+	# executed when user search's a tag
 	def create
+		# get start time
 		start_time = Time.now.getutc
 		@@browser=Watir::Browser.new :chrome
+
 		if params[:search]
 			tag=params[:search]
+			# store search history
 			History.create(
 					searchtag: tag
 					)
@@ -40,7 +53,10 @@ class ArticlesController < ApplicationController
 			session[:count] = 0
 			session[:last_inserted] = 0
 			crawl
+
+		# get finish time
 		finish_time = Time.now.getutc
+		# time taken = finish_time - start_time
 		@@time_taken_to_crawl = finish_time - start_time
 		redirect_to articles_path
 
@@ -49,6 +65,7 @@ class ArticlesController < ApplicationController
 
 	private
 
+	# logic to crawl the data
 	def crawl
 		@divs = @@browser.divs(:class => "cardChromeless u-marginTop20 u-paddingTop10 u-paddingBottom15 u-paddingLeft20 u-paddingRight20")
 		if @divs.length<=session[:count]
@@ -56,6 +73,8 @@ class ArticlesController < ApplicationController
 		end
 		@divs[session[:count]..-1].each do |a|
 			
+
+			# get details for each article
 			content=[]
 			name=(a.a(:class => "ds-link ds-link--styleSubtle link link--darken link--accent u-accentColor--textNormal u-accentColor--textDarken").text)
 			time=(a.span(:class => "readingTime").title)
@@ -69,6 +88,7 @@ class ArticlesController < ApplicationController
 				content.append(para.text)
 			end	
 
+			# store article name, title, date, time, content
 			article = Article.create(
 				name: name,
 				title: title,
@@ -77,6 +97,7 @@ class ArticlesController < ApplicationController
 				content: content
 				)
 
+			# store tags related to that article
 			@@browser.as(:href, /tag/).each do |tag|
 				Tag.create(
 					article_id: article.id,
@@ -84,7 +105,7 @@ class ArticlesController < ApplicationController
 					)
 			end
 
-			
+			# store responses of that article
 			@@browser.as(href: /responses/).each do |res|
 				@@browser.goto(res.href)
 				sleep(0.7)
